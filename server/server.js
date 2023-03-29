2900750c7fb2d9cbe7739f7c4414ea79a89f5da7
Suhan Prinivaci <galanticgd@gmail.com>
2023-03-29 02:56:10 +0000

    Update 5 files

--- server/server.js
+++ server/server.js
@@ -1,115 +1,156 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const path = require('path');
-const { writeFile } = require('fs');
+const path = require("path");
+const fs = require("fs");
+const { writeFile, readFile } = fs;
+const buffer = require("buffer");
 
 // https://expressjs.com/en/starter/static-files.html STILL MUST USE path (for some reason it requires absolute path)
-app.use(express.static(path.join('../public')));
-app.use(express.static(path.join('../images')));
+app.use(express.static(path.join("../public")));
+app.use(express.static(path.join("../images")));
 
-app.get('/', (req, res) => {
-
-    // YOU MUST USE path TO JOIN PATH BREADCRUMBS
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+app.get("/", (req, res) => {
+  // YOU MUST USE path TO JOIN PATH BREADCRUMBS
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
-app.get('/faq', (req, res) => {
-
-    // YOU MUST USE path TO JOIN PATH BREADCRUMBS
-    res.sendFile(path.join(__dirname, '../public', 'faq.html'));
+app.get("/faq", (req, res) => {
+  // YOU MUST USE path TO JOIN PATH BREADCRUMBS
+  res.sendFile(path.join(__dirname, "../public", "faq.html"));
 });
-app.get('/media', (req, res) => {
+app.get("/media", (req, res) => {
+  // figure out how to include the params
 
-    // figure out how to include the params
+  console.log(req.query.src);
+  console.log(req.query.origin);
 
-    console.log(req.query.src);
-    console.log(req.query.origin);
+  // YOU MUST USE path TO JOIN PATH BREADCRUMBS
+  res.sendFile(path.join(__dirname, "../public", "media.html"));
+});
+app.get("/login", (req, res) => {
+  // figure out how to include the params
 
-    // YOU MUST USE path TO JOIN PATH BREADCRUMBS
-    res.sendFile(path.join(__dirname, '../public', 'media.html'));
+  console.log(req.query.origin);
 
-
-
+  // YOU MUST USE path TO JOIN PATH BREADCRUMBS
+  res.sendFile(path.join(__dirname, "../public", "login.html"));
 });
 
 var userCount = 0;
+var currentAnswer = "";
+var answers = [];
+var chosenPrompts = [];
+var playersIndex = {};
 
-function emitToSocket(srcSocket, name,value,value2) {
-    io.to(srcSocket.id).emit(name,value,value2);
+function emitToSocket(srcSocket, name, value, value2) {
+  io.to(srcSocket.id).emit(name, value, value2);
 }
 
-io.on('connection', (socket) => {
-    
-    userCount += 1;
+// MAKE IT SO THEY HAVE TO CHOOSE A USERNAME
 
-    console.log('a user connected:',userCount);
+io.on("connection", (socket) => {
+  userCount += 1;
 
-    socket.on('checkAnswer', (msg) => {
-        console.log('message: ' + msg);
-        if(msg == 'hi'){
-            socket.emit('answerStatus','Correct');
-        }else{
-            socket.emit('answerStatus','Wrong');
-        };
-    });
+  console.log("a user connected:", userCount);
 
-    socket.on("upload", (file) => {
+  socket.on("setIndexes", () => {
+    if (!playersIndex[socket.id]) {
+      playersIndex[socket.id] = {};
+    }
 
-        try {
+    playersIndex[socket.id].index = 0;
+  });
 
-            var chosenPrompts = [];
-            const char = '§';
-            
-            function getRandomMessage(list) {
+  socket.on("setUsername", (newUsername) => {
+    playersIndex[toString(socket.id)].username = `${newUsername}`;
+  });
 
-                // console.log(list)
-                
-                const randomNumber = Math.round(Math.random()*list.length)
-                // console.log(randomNumber)
-                const randomItem = list[randomNumber]
-                // console.log(randomItem)
+  // CHECK PLAYER ANSWER
+  socket.on("checkAnswer", (msg, playerName) => {
+    console.log(msg);
+    io.emit("loadMessage", msg, playerName);
+  });
 
-                if(chosenPrompts.find(obj => obj == randomItem)){
-                    getRandomMessage(list)
-                } else {
-                    chosenPrompts.push(randomItem)
-                };
+  socket.on("upload", (file) => {
+    try {
+      const char = "§";
 
-            };
-            
-            getRandomMessage(file);
+      function getRandomMessage(list) {
+        // console.log(list)
 
-            chosenPrompts.forEach(msg => {
+        const randomNumber = Math.round(Math.random() * list.length);
+        // console.log(randomNumber)
+        const randomItem = list[randomNumber];
+        // console.log(randomItem)
 
-                if(msg.includes(char)){
-                    emitToSocket(socket,'generated',msg,true);
-                }else{
-                    
-                    console.log('no ref');
+        if (chosenPrompts.find((obj) => obj == randomItem)) {
+          getRandomMessage(list);
+        } else {
+          chosenPrompts.push(randomItem);
+        }
+      }
 
-                    emitToSocket(socket,'generated',msg);
+      getRandomMessage(file);
 
-                }
-                
-            });
+      // load prompts
+      chosenPrompts.forEach((msg) => {
+        const author = msg.split(",")[msg.split(",").length - 1];
 
-        } catch (error) {console.log(error); socket.emit("toast",'Please supply a working .json.'); socket.emit('reset',null);}
+        console.log(author);
+        answers.push(author);
 
-    })
+        if (msg.includes(char)) {
+          console.log("Message Contains Reference");
+          io.emit("generated", msg, true);
+        } else {
+          io.emit("generated", msg);
+        }
+      });
 
+      io.emit("setIndexes");
+    } catch (error) {
+      console.log(error);
+      socket.emit("toast", "Please supply a working .json.");
+      socket.emit("reset", null);
+    }
+  });
 
+  const usersFile = "./users.caygrey";
 
-    socket.on('disconnect', () => {
+  //code request token
+  socket.on("requestToken", (username, password) => {
+    const file = fs.readFileSync(usersFile, { encoding: "utf8" });
+    if (file == undefined || file == "") {
+      const buf = new Buffer.from("{}");
+      fs.writeFileSync(usersFile, buf.toString("base64"), { encoding: "utf8" });
+      return;
+    }
+    const users = JSON.parse(Buffer.from(file, "base64").toString("ascii"));
+    if (users[username] == undefined) {
+      // convert all file to from base then back to base
+      // binary data should be performed using Buffer.from(str, 'base64') and buf.toString('base64').
+      users[username] = password;
+      var buf = new Buffer.from(JSON.stringify(users));
+      fs.writeFileSync(usersFile, buf.toString("base64"), { encoding: "utf8" });
+    } else if (password == users[username]) {
+      const token =
+        Math.random().toString(36).substring(2) +
+        Math.random().toString(36).substring(2);
+      socket.emit("receiveToken", token);
+    }
+    console.log(users);
+  });
 
-        userCount -= 1;
+  socket.on("disconnect", () => {
+    userCount -= 1;
 
-        console.log('user disconnected',userCount);
-    });
+    console.log("user disconnected", userCount);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
